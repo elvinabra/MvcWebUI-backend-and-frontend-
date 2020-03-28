7a70845797eb86e84fb9d61a3e164262058ce90a
elvinabra <50995350+elvinabra@users.noreply.github.com>
2020-03-29 00:00:57 +0400

    Add To Cart
    Every order we add to the shopping cart is saved to our database

--- MvcWebUI-backend-and-frontend-/Abcd/MvcWebUI/Entity/DataContext.cs
+++ MvcWebUI-backend-and-frontend-/Abcd/MvcWebUI/Entity/DataContext.cs
@@ -14,6 +14,8 @@ namespace MvcWebUI.Entity
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
 
     }
 }
---
+++ MvcWebUI-backend-and-frontend-/Abcd/MvcWebUI/Models/ShippingDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcWebUI.Models
+{
+    public class ShippingDetails
+    {
+
+      
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter the address title")]
+        public string AdressTitle { get; set; }
+        [Required(ErrorMessage = "Please enter the address")]
+        public string Adress{ get; set; }
+        [Required(ErrorMessage = " Please enter your city")]
+        public string City { get; set; }
+        [Required(ErrorMessage = " Please enter your district")]
+        public string District { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
